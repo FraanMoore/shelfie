52d80457b857e101375e9b945bb0413038b030e8
FraanMoore <fraanmoore@gmail.com>
2026-05-12 17:47:05 -0400

    create routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-<h1 className="text-3xl font-bold text-gray-800">Welcome to the Frontend!</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="/books" element={<div>Books</div>} />
+      <Route path="/movies" element={<div>Movies</div>} />
+      <Route path="/series" element={<div>Series</div>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
