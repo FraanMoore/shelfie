a2b051f1907627d64c581b1ac70cf9532b57b602
FraanMoore <fraanmoore@gmail.com>
2026-06-04 18:55:39 -0400

    add heroicons library, change name app, and set colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    ".index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: [".index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        shelfie: {
+          dark: "#02333b",
+          darkest: "#012a33",
+          medium: "#174D4D",
+          mediumDark: "#38838a",
+          mediumLight: "#69aeb3",
+          mediumLightest: "#a7d6d9",
+          white: "#FFFFFF",
+          light: "#F8F9F9",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
